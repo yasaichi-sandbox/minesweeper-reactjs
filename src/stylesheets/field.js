411bece69804a4fcd7befe55a0671e45f737d0e9
yasaichi <yasaichi@users.noreply.github.com>
2015-07-02 00:57:08 +0900

    level-1 implement Field and FieldStyle

---
+++ src/stylesheets/field.js
@@ -0,0 +1,14 @@
+const FieldStyle = {
+  base: {
+    borderLeft: 'solid 1px black',
+    borderTop: 'solid 1px black'
+  },
+  shape(nRow, nCol) {
+    return {
+      width: `${46 * nCol}px`,
+      height: `${46 * nRow}px`
+    }
+  }
+};
+
+export default FieldStyle;
